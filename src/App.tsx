04bffec3cf54face8ff4820bc5296e0bb4f79250
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-21 17:28:05 -0300

    feat:Initialing the project and estructuring components and css

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Passos</p>
+        <form action="">
+          <div className="input-container"></div>
+          <div className="actions">
+            <button>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
